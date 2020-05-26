e4846463e77a7d3a7eccc2257d22c44b16fecf43
Earendilli <erenerdilli@gmail.com>
2020-05-26 17:56:33 +0300

    Implemented configurations for handling requests

--- src/main/java/com/erenerdilli/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/erenerdilli/demo/dao/FakePersonDataAccessService.java
@@ -1,11 +1,13 @@
 package com.erenerdilli.demo.dao;
 
 import com.erenerdilli.demo.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonDAO {
 
 
@@ -15,5 +17,10 @@ public class FakePersonDataAccessService implements PersonDAO {
     public int insertPerson(UUID id, Person person) {
         DB.add(new Person(id, person.getName()));
         return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return DB;
     }
 }
