dd4dd5a0e3abd3eec01eafeb24ed4e2ac721f11c
Earendilli <erenerdilli@gmail.com>
2020-05-26 16:44:29 +0300

    Initial commit

---
+++ src/main/java/com/erenerdilli/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.erenerdilli.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
